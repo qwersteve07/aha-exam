95f008483e04b1d381378648f9328206094c5c7c
SteveLee <qwersteve07@gmail.com>
2023-07-26 13:54:45 +0800

    feat: add footer to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import styles from './index.module.sass';
 import Navbar from './components/navbar';
+import Footer from './components/footer';
 
 function App() {
+  const location = useLocation()
+  const isHome = location.pathname === '/'
+
   return (
     <div className={styles.app}>
-      <Navbar className={styles.navbar} />
+      <Navbar />
       <main className={styles.main}>
         <Outlet />
       </main>
+      {isHome && <Footer />}
     </div>
   );
 }
