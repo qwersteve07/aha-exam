074d54414cec9e73eb3d13547ad6a9834ee9fe27
SteveLee <qwersteve07@gmail.com>
2023-07-26 13:57:53 +0800

    feat: add api data to follows component

--- src/components/follows/index.tsx
+++ src/components/follows/index.tsx
@@ -1,94 +1,124 @@
-import { useState } from "react";
-import styles from "./index.module.sass";
-import Button from "../button";
+import { useEffect, useState } from 'react';
+import styles from './index.module.sass';
+import Button from '../button';
+import { UserType } from '../../types/user';
+import Loading from '../loading';
+import api from '../../services/api-source';
+import mochiAvaterImage from '../../assets/mock-avater-image.jpeg'
 
 const tabs = [
-  { id: "followers", name: "Followers" },
-  { id: "following", name: "Following" },
+  { id: 'followers', name: 'Followers' },
+  { id: 'following', name: 'Following' },
 ];
 
-const mockList = [
-  {
-    avatar: "",
-    name: "John Doe",
-    id: "@johndoe",
-    status: "following",
-  },
-  {
-    avatar: "",
-    name: "John Doe",
-    id: "@johndoe",
-    status: "follow",
-  },
-  {
-    avatar: "",
-    name: "John Doe",
-    id: "@johndoe",
-    status: "following",
-  },
-  {
-    avatar: "",
-    name: "John Doe",
-    id: "@johndoe",
-    status: "follow",
-  },
-  {
-    avatar: "",
-    name: "John Doe",
-    id: "@johndoe",
-    status: "follow",
-  },
-];
+export type FollowDataProps = {
+  followers: {
+    page: number,
+    pageSize: number,
+    total: number,
+    totalPages: number
+    data: UserType[]
+  };
+  following: {
+    page: number,
+    pageSize: number,
+    total: number,
+    totalPages: number
+    data: UserType[]
+  }
+}
 
 type FollowsProps = {
   className: string;
+  // data: FollowDataProps
 }
 
-const Follows = ({ className }: FollowsProps) => {
+function Follows({ className }: FollowsProps) {
   const [currentTab, setCurrentTab] = useState(tabs[0].id);
+  const [currentData, setCurrentData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true)
+      setCurrentData([])
+      let data;
+      if (currentTab === 'followers') {
+        data = await loadFollowers(currentPage);
+      } else {
+        data = await loadFollowings(currentPage)
+      }
+      setCurrentData(data.data);
+      setTotalPages(data.totalPages)
+      setLoading(false)
+    }
+
+    loadData()
+  }, [currentTab]);
+
+  const loadFollowers = async (page: number) => {
+    return await api.fetchFollowers(page);
+  }
+
+  const loadFollowings = async (page: number) => {
+    return await api.fetchFollowings(page);
+  }
 
   return (
     <aside className={`${className} ${styles.follows}`}>
       <ul className={styles.tabs}>
-        {tabs.map((tab) => {
-          return (
-            <li
-              className={tab.id === currentTab ? styles.active : ''}
-              onClick={() => setCurrentTab(tab.id)}
-            >
-              {tab.name}
-            </li>
-          );
-        })}
+        {tabs.map((item) => <Tab currentTab={currentTab} setCurrentTab={(t) => {
+          setCurrentTab(t)
+          setCurrentPage(1)
+        }} tab={item} />)}
       </ul>
       <ul className={styles.list}>
-        {mockList
-          .filter((item) => {
-            if (currentTab === "followers") return true;
-            return item.status === "following";
-          })
-          .map((item) => {
-            return (
-              <li>
-                <div className={styles.user}>
-                  <div className={styles.avatar} />
-                  <div className={styles.info}>
-                    <div className={styles.name}>{item.name}</div>
-                    <div className={styles.id}>{item.id}</div>
-                  </div>
-                </div>
-                <Button
-                  type={item.status === "follow" ? "outlined" : "contained"}
-                  onClick={() => { }}
-                >
-                  {item.status === "follow" ? "Follow" : "Following"}
-                </Button>
-              </li>
-            );
-          })}
+        {currentData.map((item: UserType) => <User user={item} />)}
+        {loading && <Loading />}
       </ul>
     </aside>
   );
-};
+}
+
+const Tab = ({ currentTab, setCurrentTab, tab }
+  : {
+    currentTab: string,
+    setCurrentTab: (t: string) => void,
+    tab: { id: string, name: string }
+  }) => {
+  return (
+    <li
+      className={tab.id === currentTab ? styles.active : ''}
+      onClick={() => setCurrentTab(tab.id)}
+    >
+      {tab.name}
+    </li>
+  )
+}
+
+const User = ({ user }: { user: UserType }) => {
+  return (
+    <li>
+      <div className={styles.user}>
+        <div className={styles.avatar}><img src={mochiAvaterImage} /></div>
+        <div className={styles.info}>
+          <div className={styles.name}>{user.name}</div>
+          <div className={styles.id}>
+            @
+            {user.id}
+          </div>
+        </div>
+      </div>
+      <Button
+        type={user.isFollowing ? 'contained' : 'outlined'}
+        onClick={() => { }}
+      >
+        {user.isFollowing ? 'Following' : 'Follow'}
+      </Button>
+    </li>
+  )
+}
 
 export default Follows;
